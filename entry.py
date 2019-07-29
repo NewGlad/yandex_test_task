346aaf01f1189d5373da96d12080ce262ff500df
Alexey Nikitin <siriousbiz@yandex.ru>
2019-07-29 11:52:59 +0500

    Add argparse with default host/port values

--- entry.py
+++ entry.py
@@ -1,7 +1,26 @@
+import argparse
+import asyncio
 import aiohttp
+import uvloop
+
 from app import create_app
 
+parser = argparse.ArgumentParser(description='Start REST service')
+parser.add_argument(
+    '--host',
+    help='Host to listen',
+    default='0.0.0.0'
+)
+parser.add_argument(
+    '--port',
+    help='Port to listen',
+    default=8080
+)
+args = parser.parse_args()
+
+
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 app = create_app()
 
 if __name__ == '__main__':
-    aiohttp.web.run_app(app)
+    aiohttp.web.run_app(app, host=args.host, port=args.port)
