9a5ec3e02054edbb7e3e5a6e85d9d0064fb2a289
Alexey Nikitin <siriousbiz@yandex.ru>
2019-08-02 08:55:42 +0500

    add  handler for get all citizens info by import

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,4 @@ from aiohttp import web
 def setup_routes(app):
     app.router.add_route('POST', '/imports', imports.recieve_import_data)
     app.router.add_route('PATCH', '/imports/{import_id:\d+}/citizens/{citizen_id:\d+}', imports.update_citizen_info)
-    
+    app.router.add_route('GET', '/imports/{import_id:\d+}/citizens', imports.get_all_citizens)
--- app/views/imports.py
+++ app/views/imports.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from aiohttp import web
 from webargs import fields
 from webargs.aiohttpparser import parser, use_args
@@ -194,3 +195,32 @@ async def update_citizen_info(request, args):
             updated_citizen_info['relatives'] = [item[0] for item in relatives]
 
     return web.json_response(updated_citizen_info)
+
+
+async def get_all_citizens(request):
+    import_id = int(request.match_info['import_id'])
+    async with request.app['db'].acquire() as connection:
+        result = await connection.fetch('''
+        SELECT town, street, building, apartment, name, birth_date, gender, citizen_id
+        FROM citizen_info WHERE import_id = $1;
+        ''', import_id)
+        if len(result) == 0:
+            return web.Response(status=400, text='Import with given id does not exist')
+
+        result = [dict(result_item) for result_item in result]
+
+        relatives = await connection.fetch('''
+        SELECT citizen_id, relation_id FROM citizen_relation WHERE import_id = $1;
+        ''', import_id)
+
+        relatives_dict = defaultdict(list)
+        for item in relatives:
+            relatives_dict[item['citizen_id']].append(item['relation_id'])
+        
+        for citizen_info_dict in result:
+            citizen_id = citizen_info_dict['citizen_id']
+            relatives_list = relatives_dict.get(citizen_id, [])
+            citizen_info_dict['relatives'] = relatives_list
+
+
+    return web.json_response(result)
