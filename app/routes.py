aeff75b8b2ee28aa357f9d19c3763160bee51af4
Alexey Nikitin <siriousbiz@yandex.ru>
2019-08-07 17:27:07 +0500

    add routes for counting percentiles and birthdays handlers

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from .views import imports
+from .views import imports, birthdays, stat
 from aiohttp import web
 def setup_routes(app):
     app.router.add_route('POST', '/imports', imports.recieve_import_data)
     app.router.add_route('PATCH', '/imports/{import_id:\d+}/citizens/{citizen_id:\d+}', imports.update_citizen_info)
     app.router.add_route('GET', '/imports/{import_id:\d+}/citizens', imports.get_all_citizens)
+    app.router.add_route("GET", '/imports/{import_id:\d+}/citizens/birthdays', birthdays.count_birthdays)
+    app.router.add_route("GET", "/imports/{import_id:\d+}/towns/stat/percentile/age", stat.count_percentile)
